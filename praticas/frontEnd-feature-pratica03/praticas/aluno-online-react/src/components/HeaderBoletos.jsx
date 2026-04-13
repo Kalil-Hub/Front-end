bfadc636b7521747540b7978c089856b1cf88555
Kalil Lima <4leadk@gmail.com>
2026-04-13 10:12:56 -0300

    Arrumando problema

--- praticas/frontEnd-feature-pratica03/praticas/aluno-online-react/src/components/HeaderBoletos.jsx
+++ praticas/frontEnd-feature-pratica03/praticas/aluno-online-react/src/components/HeaderBoletos.jsx
@@ -1,7 +1,7 @@
-export default function HeaderNotas() {
+export default function HeaderBoletos() {
   return (
     <header className="flex justify-between items-center mb-10">
-      <h1 className="text-[2rem] font-bold text-[#202020]">Minhas Notas</h1>
+      <h1 className="text-[2rem] font-bold text-[#202020]">Meus Boletos</h1>
       <img src="/avatar.svg" alt="Avatar do usuário" className="w-10 h-10 rounded-full object-cover" />
     </header>
   );
--- praticas/frontEnd-feature-pratica03/praticas/aluno-online-react/src/components/HeaderDashboard.jsx
+++ praticas/frontEnd-feature-pratica03/praticas/aluno-online-react/src/components/HeaderDashboard.jsx
@@ -1,7 +1,7 @@
-export default function HeaderNotas() {
+export default function HeaderDashboard() {
   return (
     <header className="flex justify-between items-center mb-10">
-      <h1 className="text-[2rem] font-bold text-[#202020]">Minhas Notas</h1>
+      <h1 className="text-[2rem] font-bold text-[#202020]">Olá, Aluno!</h1>
       <img src="/avatar.svg" alt="Avatar do usuário" className="w-10 h-10 rounded-full object-cover" />
     </header>
   );
