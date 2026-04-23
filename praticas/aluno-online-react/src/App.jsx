9e14134dc956c6ecbf1459eb54d0eb557a40c0cf
Kalil Lima <4leadk@gmail.com>
2026-04-23 09:51:58 -0300

    Arrumando conflitos pratica05

---
+++ praticas/aluno-online-react/src/App.jsx
@@ -0,0 +1,19 @@
+import Dashboard from './pages/Dashboard';
+import Faltas from './pages/Faltas';
+import Notas from './pages/Notas';
+import Boletos from './pages/Boletos';
+import Requerimentos from './pages/Requerimentos';
+import Login from './Pages/Login';
+
+export default function App() {
+  return (
+    <>
+      <Login />
+      {/*<Dashboard />*/}
+      {/*<Notas />*/}
+      {/* <Faltas /> */}
+      {/* <Boletos /> */}
+      {/* <Requerimentos /> */}
+    </>
+  );
+}
